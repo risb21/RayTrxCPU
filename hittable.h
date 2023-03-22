f88dedbba5e2d5bf52d95896e07fab57a971a50c
Rishabh Chopde <rishikumarikuf@gmail.com>
2023-03-22 20:45:32 +0530

    Abstracted sphere into a hittable object

---
+++ hittable.h
@@ -0,0 +1,19 @@
+#pragma once
+
+#ifndef HITTABLE_H
+#define HITTABLE_H
+
+#include "ray.h"
+
+struct hit_record {
+    point3 p;
+    vec3 normal;
+    float t;
+};
+
+class hittable {
+public:
+    virtual bool hit(const ray& r, float t_min, float t_max, hit_record& rec) const = 0;
+};
+
+#endif
